ac093de696eba009cd852f853bdaba2aea969e1d
Kenneth Phang <bunnyppl@gmail.com>
2018-02-19 14:23:09 +0800

    user registration

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-
+import { RegistrationUser } from './shared/registration-user'
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -7,9 +7,29 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'app';
+  model = new RegistrationUser('','','','','','',null,'SG','','');
+  gender: string[] = ['M', 'F'];;
+  nationalities = [ {desc:'Singapore', value:'SG'}, 
+                    {desc: 'Malaysia', value:'MY'}, 
+                    {desc:'Thailand', value:'TH'},  
+                    {desc:'Vietnam', value: 'VN'}];
+  isSubmitted: boolean = false;
+  result: string = "";
 
   // this is to handle when I click on a submit or save button.
   onSubmit(){
-
+    console.log(this.model.email);
+    console.log(this.model.password);
+    console.log(this.model.confirmPassword);
+    console.log(this.model.firstName);
+    console.log(this.model.lastName);
+    console.log(this.model.gender);
+    console.log(this.model.dateOfBirth);
+    console.log(this.model.address);
+    console.log(this.model.nationality);
+    console.log(this.model.contactNumber);
+    this.result = JSON.stringify(this.model);
+    this.isSubmitted = true;
+    
   }
 }
