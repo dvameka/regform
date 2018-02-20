6ae9341df24c23a336cab899aae409c0e1e87bb6
Kenneth Phang <bunnyppl@gmail.com>
2018-02-20 14:03:13 +0800

    with validation

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,21 +1,27 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { RegistrationUser } from './shared/registration-user'
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
   title = 'app';
-  model = new RegistrationUser('','','','','','',null,'','SG','');
+  model = null;
   gender: string[] = ['M', 'F'];
+  
   nationalities = [ {desc:'Singapore', value:'SG'}, 
                     {desc: 'Malaysia', value:'MY'}, 
                     {desc:'Thailand', value:'TH'},  
                     {desc:'Vietnam', value: 'VN'}];
+                    
   isSubmitted: boolean = false;
   result: string = "";
-
+  
+  ngOnInit() {
+    this.model = new RegistrationUser('','','','','','',null,'','MY','');
+  }
+  
   // this is to handle when I click on a submit or save button.
   onSubmit(){
     console.log(this.model.email);
@@ -28,9 +34,7 @@ export class AppComponent {
     console.log(this.model.address);
     console.log(this.model.nationality);
     console.log(this.model.contactNumber);
-    this.result = JSON.stringify(this.model);
     this.isSubmitted = true;
-    
   }
 
   onChange(event){
